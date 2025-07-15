37a449dfc58728c7d1fc1fd95b754ebc94ac34b3
pavani-2002-max <85422285+pavani-2002-max@users.noreply.github.com>
2025-07-15 22:12:23 +0530

    shimmer.jsx

---
+++ shimmer.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+
+function Shimmer() {
+  return (
+    <div className='col-10'> 
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+<div class="card border-0" aria-hidden="true">
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+    </h5>
+    <p class="card-text placeholder-glow">
+      <div class="placeholder col-12" style={{height: "220px"}}></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+      <div class="placeholder col-12"></div>
+    </p>
+  </div>
+
+</div>
+</div>
+</div>
+
+
+
+      
+    
+  )
+}
+
+export default Shimmer
